a04564307ad4ed50656f69a47e8cb9cddb7ddaee
Lewis Anggi <lewis.anggi@bps.go.id>
2022-08-18 16:49:28 +0700

    Preview ke index dashboard

--- resources/views/kelolagambar/index.blade.php
+++ resources/views/kelolagambar/index.blade.php
@@ -281,7 +281,68 @@
                             </div>
                             <div class="modal-body">
                                 <div class="">
-                                    @include('KelolaGambar.preview')
+                                    
+<div class="row">
+    <div class="col-12">
+        <div class="card-body">
+            
+            <div class="row mb-3">
+                <div class="font-weight-bold col-sm-3 col-form-label">
+                    <div class="d-flex flex-row-reverse text-info">Judul :</div>
+                </div>
+                <div class="col-sm-9">
+                    <label class="col-sm-12 col-form-label">
+                        <span class="previewJudul"></span>
+                    </label>             
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="font-weight-bold col-sm-3 col-form-label">
+                    <div class="d-flex flex-row-reverse text-info">Link :</div>
+                </div>
+                <div class="col-sm-9">
+                    <label class=" col-sm-12 col-form-label">
+                        <span class="previewLink"></span>
+                    </label>                  
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="font-weight-bold col-sm-3 col-form-label">
+                    <div class="d-flex flex-row-reverse text-info">Tanggal Permintaan :</div>
+                </div>
+                <div class="col-sm-9">
+                    <label class=" col-sm-12 col-form-label">
+                        <span class="previewWaktu"></span>
+                    </label>                  
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="font-weight-bold col-sm-3 col-form-label">
+                    <div class="d-flex flex-row-reverse text-info">Kegunaan :</div>
+                </div>
+                <div class="col-sm-9">
+                    <label class=" col-sm-12 col-form-label">
+                        <span class="previewKegunaan"></span>  
+                    </label>               
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="font-weight-bold col-sm-3 col-form-label">
+                    <div class="d-flex flex-row-reverse text-info">Status :</div>
+                </div>
+                <div class="col-sm-9">
+                    <label class=" col-sm-2 col-form-label">
+                        <span class="previewStatus"></span>
+                    </label>              
+                </div>
+            </div>
+                            
+            <span class="previewAlasanTolak"></span>
+        </div>
+    </div>  
+</div> 
+
+
                                 </div>
                             </div>
                         </div>
