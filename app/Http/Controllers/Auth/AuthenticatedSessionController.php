30c49d051bee0b465a441405be6ee617cb7164a4
lewis.anggi <lewis.anggi@bps.go.id>
2022-10-07 11:57:45 +0700

    Menambahkan Login SSO - Complete

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -1,12 +1,15 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
+use Illuminate\Support\Facades\Auth as Auths;
 use \JKD\SSO\Client\Provider\Keycloak;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -52,26 +55,31 @@ class AuthenticatedSessionController extends Controller
         return redirect('login');
     }
 
-    public function actionSso()
+    public function actionSso(Request $request)
     {
+
          /** Implement SSO Dec 2020 */
         $provider = new Keycloak([
             'authServerUrl'         => 'https://sso.bps.go.id',
             'realm'                 => 'pegawai-bps',
             'clientId'              => '03200-simrad-97h',
             'clientSecret'          => 'bed2e9b7-896e-489d-b3ab-0fed95d673b4',
-            'redirectUri'           => 'http://localhost:81/simbrs2/web/site/sso'
+            'redirectUri'           => 'http://localhost:8000/login',
             //'redirectUri'           => 'http://localhost/site/sso'
         ]);
         
-        if(!isset($_GET['code'])){
+        if (!isset($_GET['code'])) {
+
+            // Untuk mendapatkan authorization code
             $authUrl = $provider->getAuthorizationUrl();
-            $_SESSION['oauth2state'] = $provider->getState();
+            $request->session()->put('oauth2state', $provider->getState());
             header('Location: '.$authUrl);
             exit;
-        }else if(empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
-            unset($_SESSION['oauth2state']);
-            exit('Invalid state');
+
+        } elseif (empty($_GET['state']))
+            {
+                $request->session()->forget('oauth2state');
+                exit('Invalid state');
         }
         else {
             try {
@@ -84,35 +92,39 @@ class AuthenticatedSessionController extends Controller
             
             // Opsional: Setelah mendapatkan token, anda dapat melihat data profil pengguna
             try {
-
-                $user = $provider->getResourceOwner($token);
                 
-                $searchUser = \app\models\User::find()->where(['username'=>$user->getUsername()])->one();
+                $user = $provider->getResourceOwner($token);
 
-                $modelUser = new \app\models\User();
-                $modelUser->name = $user->getName();
-                $modelUser->email = $user->getEmail();
-                $modelUser->username = $user->getUsername();
-                $modelUser->level = 4;
-                $modelUser->nip = $user->getNip();
-                $modelUser->nipbaru = $user->getNipBaru();
-                $modelUser->alamatkantor = $user->getAlamatKantor();
-                $modelUser->golongan = $user->getGolongan();
-                $modelUser->jabatan = $user->getJabatan();
-                $modelUser->profilepicture = $user->getUrlFoto();
-                $modelUser->satker = $user->getEselon();
-                $salt = $this->generateSalt();
-                $modelUser->salt = $salt;
-                $modelUser->password = $this->hashPassword($salt, 'pks2022');
-                if(!$searchUser)
-                {
-                    $modelUser->save();
-                    Yii::$app->user->login($modelUser);
-                }else{
-                    $usr = \app\models\User::findOne($searchUser->id);
-                    Yii::$app->user->login($usr);
+
+                $email = $user->getEmail();
+                $id = User::where('email', $email)->first();
+
+                if (!empty($id)) {
+                    $id = $id->id;
+                } else {
+                    $newUser = User::create([
+                        'name' => $user->getName(),
+                        'email' => $user->getEmail(),
+                        'level' => 4,
+                        'nip' => $user->getNip(),
+                        'nipbaru' => $user->getNipBaru(),
+                        'alamatkantor' => $user->getAlamatKantor(),
+                        'golongan' => $user->getGolongan(),
+                        'jabatan' => $user->getJabatan(),
+                        'profilepicture' => $user->getUrlFoto(),
+                        'satker' => $user->satker(),
+                        'password' =>Hash::make('pks2022'),
+                    ]);
+                    $id = $newUser->id;
                 }
-                return $this->redirect(['login']);
+
+                // Login dengan menggunakan id pengguna dari record di database aplikasi
+                if (Auths::loginUsingId($id)) {
+                    return redirect()->intended('/');
+                } else {
+                    return redirect('/');
+                }
+
             } catch (\Exception $e) {
                 exit('Gagal Mendapatkan Data Pengguna: '.$e->getMessage());
             }
