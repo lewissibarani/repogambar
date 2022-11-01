138e6fbf965439410052eec58793af57a7648e54
lewis.anggi <lewis.anggi@bps.go.id>
2022-11-01 12:57:02 +0700

    Menambahkan Logo Pikart dan Nama

--- resources/views/_layout/nav.blade.php
+++ resources/views/_layout/nav.blade.php
@@ -3,10 +3,10 @@
     <div class="logo position-relative">
         <a href="/">
             <!-- Logo can be added directly -->
-            <!-- <img src="/img/logo/logo-white.svg" alt="logo" /> -->
+            <img src="/img/logo/logo-white.svg" alt="logo" />
 
             <!-- Or added via css to provide different ones for different color themes -->
-            <div class="img"></div>
+            <!-- <div class="img"></div> -->
         </a>
     </div>
     <!-- Logo End -->
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,7 +10,7 @@
 <head>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
-    <title> SIMRAD | {{$title}}</title>
+    <title> Pikart | {{$title}}</title>
     <meta name="description" content="{{$description}}"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @include('_layout.head')
