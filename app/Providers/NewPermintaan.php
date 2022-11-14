eab22e6d249abc5ded594dedf8586fb50963ce7d
lewis.anggi <lewis.anggi@bps.go.id>
2022-11-14 13:17:09 +0700

    add implement shoulbroadcast ke NewPermintaan

--- app/Providers/NewPermintaan.php
+++ app/Providers/NewPermintaan.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class NewPermintaan
+class NewPermintaan implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
