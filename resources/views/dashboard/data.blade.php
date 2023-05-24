f1189480f83690a6de172093ad1b2f15be416eb4
lewis.anggi <lewis.anggi@bps.go.id>
2023-05-24 14:06:56 +0700

    edit hasil pncarian dan data

--- resources/views/dashboard/data.blade.php
+++ resources/views/dashboard/data.blade.php
@@ -1,37 +1,37 @@
 @foreach ($Data as $datas)
     <div class="col">
         <div class="card hover-img-scale-up hover-reveal">
-                <img class="card-img scale" 
-                style=" height:350px;" 
+                <img class="card-img sh-50 scale"  
                 src="{{$datas->thumbnail_path}}"
                 alt="card image" />
-                <div class="card-img-overlay justify-content-between reveal-content">  
-                            <div style="
-                                position: absolute;
-                                bottom: 30px;
-                                width: 100%;">
-                                <a href="Dashboard/DetailGambar/{{$datas->id}}" class="stretched-link">
+                <div class="card-img-overlay justify-content-between reveal-content">   
+                        <div class="row g-0">
+                        </div>
+                        <div class="row g-0">
+                            <div class="col pe-2">
+                                <a href="/Dashboard/DetailGambar/{{$datas->id}}" class="stretched-link">
                                     <h5 class="heading text-white mb-1">{{$datas->judul}}</h5>
                                 </a>
-                                <div class=" ">
+                                <div class="d-inline-block">
                                     <div class="text-uppercase"><span class='badge rounded-pill bg-light'>{{$datas->tipe_gambar}}</span></div>
                                 </div>
                                 @php
                                 if($datas->file_id!==null)
                                     {
                                     @endphp
-                                    <div class=" ">
+                                    <div class="d-inline-block">
                                         <div class="text-uppercase"><span class='badge rounded-pill bg-light'>ZIP</span></div>
                                     </div>
                                     @php
                                         $file = "zip";
                                     }
                                 @endphp
-                                <div class=" ">
+                                <div class="d-inline-block">
                                     <div class="text-uppercase"><span class='badge rounded-pill bg-light'>{{$datas->source->sumber_gambar}}</span></div>
                                 </div>
                                 
-                            </div> 
+                            </div>
+                        </div>  
                 </div>
         </div>
     </div>
--- resources/views/dashboard/hasilpencarian.blade.php
+++ resources/views/dashboard/hasilpencarian.blade.php
@@ -76,37 +76,37 @@
                         <div class="col">
                             <div class="card hover-img-scale-up hover-reveal">
                                     <img class="card-img sh-50 scale" 
-                                    src="/pikart/{{$datas->thumbnail_path}}"
+                                    src="{{$datas->thumbnail_path}}"
                                     alt="card image" />
                                     <div class="card-img-overlay d-flex flex-column justify-content-between reveal-content">
-                                            <div class="row g-0">
-                                            </div>
-                                            <div class="row g-0">
-                                                <div class="col pe-2">
-                                                    <a href="/Dashboard/DetailGambar/{{$datas->id}}" class="stretched-link">
-                                                        <h5 class="heading text-white mb-1">{{$datas->judul}}</h5>
-                                                    </a>
-                                                    <div class="d-inline-block">
-                                                        <div class="text-uppercase"><span class='badge rounded-pill bg-light'>{{$datas->tipe_gambar}}</span></div>
-                                                    </div>
-                                                    @php
-                                                    if($datas->file_id!==null)
-                                                        {
-                                                        @endphp
-                                                        <div class="d-inline-block">
-                                                            <div class="text-uppercase"><span class='badge rounded-pill bg-light'>ZIP</span></div>
-                                                        </div>
-                                                        @php
-                                                            $file = "zip";
-                                                        }
+                                        <div class="row g-0">
+                                        </div>
+                                        <div class="row g-0">
+                                            <div class="col pe-2">
+                                                <a href="/Dashboard/DetailGambar/{{$datas->id}}" class="stretched-link">
+                                                    <h5 class="heading text-white mb-1">{{$datas->judul}}</h5>
+                                                </a>
+                                                <div class="d-inline-block">
+                                                    <div class="text-uppercase"><span class='badge rounded-pill bg-light'>{{$datas->tipe_gambar}}</span></div>
+                                                </div>
+                                                @php
+                                                if($datas->file_id!==null)
+                                                    {
                                                     @endphp
                                                     <div class="d-inline-block">
-                                                        <div class="text-uppercase"><span class='badge rounded-pill bg-light'>{{$datas->source->sumber_gambar}}</span></div>
+                                                        <div class="text-uppercase"><span class='badge rounded-pill bg-light'>ZIP</span></div>
                                                     </div>
-                                                    
+                                                    @php
+                                                        $file = "zip";
+                                                    }
+                                                @endphp
+                                                <div class="d-inline-block">
+                                                    <div class="text-uppercase"><span class='badge rounded-pill bg-light'>{{$datas->source->sumber_gambar}}</span></div>
                                                 </div>
+                                                
                                             </div>
                                         </div>
+                                    </div>
                             </div>
                         </div>
                         <!-- src="{{$datas->thumbnail_path}}"  -->
