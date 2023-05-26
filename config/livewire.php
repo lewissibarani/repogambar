2ae8f230fbbb1f7aeaa508a99ba3ee71ef90acf5
Lewis Anggi <lewis.anggi@bps.go.id>
2023-05-26 21:43:28 +0700

    livewire config

--- config/livewire.php
+++ config/livewire.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'asset_url' => env('APP_URL'),
+    'asset_url' => "/assets",
 
     /*
     |--------------------------------------------------------------------------
@@ -68,7 +68,7 @@ return [
     |
     */
 
-    'app_url' =>  env('APP_URL'),
+    'app_url' =>  null,
 
     /*
     |--------------------------------------------------------------------------
@@ -96,7 +96,7 @@ return [
     */
 
     'temporary_file_upload' => [
-        'disk' => 'local',        // Example: 'local', 's3'              Default: 'default'
+        'disk' => null,        // Example: 'local', 's3'              Default: 'default'
         'rules' => null,       // Example: ['file', 'mimes:png,jpg']  Default: ['required', 'file', 'max:12288'] (12MB)
         'directory' => null,   // Example: 'tmp'                      Default  'livewire-tmp'
         'middleware' => null,  // Example: 'throttle:5,1'             Default: 'throttle:60,1'
