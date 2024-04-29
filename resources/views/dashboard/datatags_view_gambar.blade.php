b76604a7326f99aa2b1f89cf494c6c438771916f
lewissibarani <lewissibarani@gmail.com>
2024-04-29 08:19:39 +0700

    memperbaiki tags

--- resources/views/dashboard/datatags_view_gambar.blade.php
+++ resources/views/dashboard/datatags_view_gambar.blade.php
@@ -1,11 +1,9 @@
-<?php $counter=0;?>
+ 
 @foreach($Tags as $tag)
 
-    <a class="btn btn-sm btn-icon btn-icon-end btn-outline-primary mb-1 me-1" href="{{route('result_pencarian' , ['katakunci'=>$tag[$counter],
-                                                                                                                  'tipeaset'=>'null',
-                                                                                                                  'tipehasil'=>'null'
+    <a class="btn btn-sm btn-icon btn-icon-end btn-outline-primary mb-1 me-1" 
+    href="{{route('result_pencarian' , ['katakunci'=>$tag,'tipeaset'=>'null','tipehasil'=>'null'
         ])}}"> 
-        <span>{{$tag[$counter]}}</span>
-    </a>
-    <?php $counter++;?>
+        <span>{{$tag}}</span>
+    </a> 
 @endforeach
